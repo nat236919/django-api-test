1eaff44fc2425a5d9a86119990c5e7b228726580
nat236919 <nuttaphat.aruno@gmail.com>
2020-12-18 16:33:35 +0800

    add User model for myapi

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=60)
+    email = models.EmailField(max_length=60)
+    catch_phrase = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.username
+    
