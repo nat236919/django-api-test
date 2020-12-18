0957b3a727988ff2afcde0e94350a9a709e8161d
nat236919 <nuttaphat.aruno@gmail.com>
2020-12-18 16:54:40 +0800

    create RESTapi for User

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
